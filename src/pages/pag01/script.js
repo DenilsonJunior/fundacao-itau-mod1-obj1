8815a1ee85b3b61306086ce28ecee424d63b34a4
nilo.cesar <nilo.cesar@afferolab.com.br>
2024-11-20 17:06:52 -0300

    FEAT: otimização

--- src/pages/pag01/script.js
+++ src/pages/pag01/script.js
@@ -1,18 +1,1 @@
-events.on('ready', function () {
-    $('.boxInteraction').each(function () {
-        const $box = $(this);
-    
-        $box.find('.btn').on('click', function () {
-            const targetSelector = $(this).data('target');
-            const $messages = $box.find('.boxMessage .msg');
-            const $buttons = $box.find('.btn');
-        
-            $messages.addClass('hide');
-            $box.find(targetSelector).removeClass('hide');
-        
-            $buttons.removeClass('open');
-            $(this).addClass('open');
-        });
-    });
-});
-  
+events.on("ready", function () {});
